019806f0c9a4096f796dad7271049490d01bcc38
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-07-30 21:38:17 +0100

    batch

---
+++ st.sh
@@ -0,0 +1,8 @@
+# 30-7-2019 JHZ
+
+git add README.md
+git commit -m "README"
+git remote add origin git@github.com:cambridge-ceu/CEU-scientific-meetings.git
+git add st.sh
+git commit -m "batch"
+git push -u origin master

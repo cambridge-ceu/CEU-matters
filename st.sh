18653ecc119e43dcc4cb6bdc0421ebb7411cd514
jinghuazhao <jinghuazhao@hotmail.com>
2020-02-03 16:02:58 +0000

    batch

--- st.sh
+++ st.sh
@@ -1,7 +1,9 @@
-# 30-7-2019 JHZ
+# 3-2-2020 JHZ
 
 git add README.md
 git commit -m "README"
+git add 2020.jpg
+git commit -m "One-page 2020 calendar"
 git remote add origin git@github.com:cambridge-ceu/CEU-scientific-meetings.git
 git add st.sh
 git commit -m "batch"

cea1262555ed7d4057c82f07efb3164ee12170ba
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2020-10-12 10:01:03 +0100

    Auxiliary files

---
+++ files/st.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+git remote add origin git@github.com:cambridge-ceu/CEU-scientific-meetings.git
+git add README.md
+git commit -m "README"
+git add files/
+git commit -m "Auxiliary files"
+git add fullcalendar-4.3.1
+git commit -m "Google Calendar"
+git add st.sh
+git commit -m "batch"
+git push -u origin master
+
+# 959151
+# 920084

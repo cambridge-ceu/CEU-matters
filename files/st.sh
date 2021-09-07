6819084d0233a36ecfa6ffb1a74e0ec23551229f
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-09-07 21:12:33 +0000

    Deployed 1d490bd with MkDocs version: 1.1.2

---
+++ files/st.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+git add .gitignore
+git commit -m ".gitignore"
+git add README.md
+git commit -m "README"
+git add files/
+git commit -m "Auxiliary files"
+git add fullcalendar-4.3.1
+git commit -m "Google Calendar"
+git add phpc-ceu-group.md
+git commit -m "CEU group mailing list"
+git add st.sh
+git commit -m "batch"
+git push -u origin master
+
+# CM357
+# 123456
+# 920084
+# 10443
+# git remote add origin git@github.com:cambridge-ceu/CEU-scientific-meetings.git
+# npm install --save vue-lunar-full-calendar
+
